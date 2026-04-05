fcc70b00368184863e5245d5515963ec99fcdb9a
dbeediv <msu10.divyabharathi@gmail.com>
2026-04-05 17:13:08 +0530

    Add campaigns route with GET and POST methods

--- routes/campaigns.js
+++ routes/campaigns.js
@@ -1,1 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+const { authMiddleware } = require('../middleware/auth');
 
+router.use(authMiddleware);
+
+router.get('/', async (req, res) => {
+  const r = await db.query('SELECT * FROM campaigns');
+  res.json(r.rows);
+});
+
+router.post('/', async (req, res) => {
+  const { name, domain, scenario } = req.body;
+
+  const r = await db.query(
+    'INSERT INTO campaigns (name, domain, scenario) VALUES ($1,$2,$3) RETURNING *',
+    [name, domain, scenario]
+  );
+
+  res.json(r.rows[0]);
+});
+
+module.exports = router;
