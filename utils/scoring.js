a20de598dbe9182246c87e198864c24bacf6eee7
dbeediv <msu10.divyabharathi@gmail.com>
2026-04-05 23:21:33 +0530

    Update scoring.js

--- utils/scoring.js
+++ utils/scoring.js
@@ -1,1 +1,7 @@
-
+export function getScoreDelta(event) {
+  if (event === "clicked") return -15
+  if (event === "submitted") return -30
+  if (event === "reported") return +15
+  if (event === "training") return +10
+  return 0
+}
