7c520b5b9e6f43be94f89b36a307972d08f26013
dbeediv <msu10.divyabharathi@gmail.com>
2026-04-05 17:12:41 +0530

    Add user routes with authentication middleware

--- routes/users.js
+++ routes/users.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db');
+const { authMiddleware } = require('../middleware/auth');
 
+router.use(authMiddleware);
+
+router.get('/', async (req, res) => {
+  const r = await db.query('SELECT * FROM users');
+  res.json(r.rows);
+});
+
+router.get('/me', async (req, res) => {
+  const r = await db.query('SELECT * FROM users WHERE id=$1', [req.user.id]);
+  res.json(r.rows[0]);
+});
+
+module.exports = router;
